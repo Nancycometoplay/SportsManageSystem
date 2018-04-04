ce84b60bf3a78434e142a429b9e454f225dff31e
Nancy1001 <16yhuang7@stu.edu.cn>
2018-04-04 11:19:15 +0800

    change_byHUANGYU branch 的开始

--- Liner_List_Operation.h
+++ Liner_List_Operation.h
@@ -2,40 +2,63 @@
 // Created by Nancy on 18/3/19.
 //
 
-#ifndef SPORTSYSTEM_2_LINER_LIST_OPERATION_H
-#define SPORTSYSTEM_2_LINER_LIST_OPERATION_H
-
-#endif //SPORTSYSTEM_2_LINER_LIST_OPERATION_H
-
-
-//#include <iostream>
-#include <stdio.h>
-#include <stdlib.h>
-#include <memory.h>
 
 #define MAX_SIZE 100
+#define comp_per_athle 3;
 //using namespace std;
 
-int findtemp=0;
+
+//#define compsNumber 20
 
 
-typedef struct SqList{
-    struct athletes *elem;  //根据程序来改变
-    int length;
-};
+//#define compsNumber 20
 
-struct athletes{
-    char name[20];
-    char id[20];
-    char gender[10];
-    char grade[10];
-    char college[20]; //学院
-    // char department[20];
-    char major[20];   //专业
-    char preteam[10]; //代表队
-    // char number[10];
-    // char score[10];
-};
+
+//typedef struct {
+//    char name[20];
+//    char id[20];
+//    char gender[10];
+//    char grade[10];
+//    char college[20]; //学院
+//    // char department[20];
+//    char major[20];   //专业
+//    char preteam[10]; //代表队
+//    // char number[10];
+//    // char score[10];
+//    //struct compitition *comp[comp_per_athle];
+//} athletes;
+
+
+int Append_List(athletes athle[],int number, athletes *addAth);
+void Delete_List(athletes athle[],int number,athletes deleteAth);
+void Chanege_List(athletes athle[],int number,athletes changeAth);
+athletes Find_List(athletes athle[], int number, int method, char* keyword );
+int isFind_List(athletes athle[], int number, int method, char* keyword );
+void Display_List(athletes athle[], int number);
+
+
+//#define comp_per_athle 3;
+#define athe_per_comp _Max 30;
+
+//#include <iostream>
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <memory.h>
+//
+//
+//
+
+
+//都是从
+
+
+
+
+//typedef struct SqList{
+//    struct athletes *elem;  //根据程序来改变
+//    int length;
+//};
+
 
 
 //int Init_List(struct SqList *L);
@@ -99,37 +122,25 @@ struct athletes{
 //    return 1;
 //}
 
-/*=====追加=====*/
-int Append_List(struct athletes athle[],int number, struct athletes addAth){
-    int i,j;
-    struct SqList *L;
-    L->elem=NULL;
-    L->length=0;
+/*=====追加=====*/  //number就是下标
+int Append_List(athletes athle[],int number, athletes *addAth){
 
-    L->elem = athle;   //不知道对不对
-    if(!L->elem) {return -1;}
-    L->length = number;
-    for(int i=0;i<L->length;i++){
-        L->elem[i]=athle[i] ;
-    }
-    L->length++;
-    L->elem[L->length-1] = addAth;
+   // L->elem[L->length-1] = addAth;
 
-    //将athle清空
-    for(int j=0;j<number;j++)
-    {
-        memset(&athle[j],0,sizeof(struct athletes));
-    }
+   if((number+1<1)||(number+1>athleteNumber))
+       return 0;
+   else{
+       athle[number+1]=*addAth;
+       return 1;
+   }
 
-    //传回athle[]
-    for(int i=0;i<L->length;i++){
-        athle[i]=L->elem[i];
-    }
 }
 
 
 /*=====删除=====*/           //whereDelete就是下标,删除就是后面的移到前面去
-void Delete_List(struct athletes athle[],int number,struct athletes deleteAth){
+
+//传入删除前的数组的地址，该数组的大小，要删除的元素
+void Delete_List(athletes athle[],int number,athletes deleteAth){
     int whereDelete=0;  //删除操作的下标
     for(int i=0;i<=number;i++)
     {
@@ -145,10 +156,10 @@ void Delete_List(struct athletes athle[],int number,struct athletes deleteAth){
         athle[i]=athle[i+1];
     }
     //删除最后一个元素
-    struct athletes *p;
-    *p=athle[number];
-    p=NULL;
-    printf("删除成功");
+//    struct athletes *p;
+//    *p=athle[number];
+//    p=NULL;
+//    printf("删除成功");
 
 
 
@@ -185,63 +196,98 @@ void Delete_List(struct athletes athle[],int number,struct athletes deleteAth){
 }
 
 /*=====修改=====*/
-int Chanege_List(struct athletes athle[], int number,int changeNum, struct athletes changeAth){
+void Chanege_List(athletes athle[],int number,athletes changeAth){
     //L->elem[num] = ele;
-    int i,j;
-    struct SqList *L;
-    L->elem=NULL;
-    L->length=0;
-
-    L->elem = athle;   //不知道对不对
-    if(!L->elem) {return -1;}
-    L->length = number;  //总共有多少个元素
-    for(int i=0;i<L->length;i++){
-        L->elem[i]=athle[i] ;
-    }
-
-    L->elem[changeNum] = changeAth;
-
-    //将athle清空
-    for(int j=0;j<number;j++)
+    int whereChange=0;  //删除操作的下标
+    for(int i=0;i<=number;i++)
     {
-        memset(&athle[j],0,sizeof(struct athletes));
+        if(athle[i].id==changeAth.id)  //找到对应的i
+        {
+            whereChange=i;
+            break;
+        }
     }
 
-    //传回athle[]
-    for(int i=0;i<L->length;i++){
-        athle[i]=L->elem[i];
-    }
-
-
-
+    athle[whereChange]=changeAth;
 }
+
 /*=====查询=====*/    //number是从0开始的
-struct athletes Find_List(struct athletes athle[], int number, int method, char* keyword )
+athletes Find_List(athletes athle[], int number, int method, char* keyword )
 {
     //int temp=0;
     //cout<<"L"<<"["<<num<<"]"<<"="<<L->elem[num-1]<<endl;
-    if(method==1)  //学号
+    if(method==1)  //用学号找运动员
     {
         for(int i=0;i<=number;i++)
         {
             if(strcmp(athle[i].id,keyword)==0)  //如果一致
             {
-                findtemp=1;
+
                 return athle[i];
             }
         }
 
         printf("没有找到！");
-        findtemp=0;
 
-        struct athletes empty;
-        struct athletes *p;
-        *p=empty;
-        p=NULL;
-        return empty ;
+
+//        struct athletes empty;
+//        struct athletes *p;
+//        *p=empty;
+//        p=NULL;
+//        return empty ;
     }
 
 }
+
+int isFind_List(athletes athle[], int number, int method, char* keyword )
+{
+    //int temp=0;
+    //cout<<"L"<<"["<<num<<"]"<<"="<<L->elem[num-1]<<endl;
+    if(method==1)  //用学号找运动员
+    {
+        for(int i=0;i<=number;i++)
+        {
+            if(strcmp(athle[i].id,keyword)==0)  //如果一致
+            {
+               // findtemp=1;
+                return 1;
+            }
+        }
+
+      //  printf("没有找到！");
+      //  findtemp=0;
+        return 0;
+
+//        struct athletes empty;
+//        struct athletes *p;
+//        *p=empty;
+//        p=NULL;
+//        return empty ;
+    }
+
+}
+
+//根据运动员学号找代表队的名称
+char* Find_List_teamName(athletes athle[], int number, int method, char* keyword , char *dest)
+{
+    //int temp=0;
+    //cout<<"L"<<"["<<num<<"]"<<"="<<L->elem[num-1]<<endl;
+    if(method==1)  //用学号找运动员对应的队伍
+    {
+        for(int i=0;i<=number;i++)
+        {
+            if(strcmp(athle[i].id,keyword)==0)  //如果一致
+            {
+                strcpy(dest,athle[i].preteam);
+                return dest;
+            }
+        }
+
+       // printf("没有找到！");
+    }
+
+}
+
 
 
 /*=====插入=====*/
@@ -257,15 +303,12 @@ struct athletes Find_List(struct athletes athle[], int number, int method, char*
 //}
 
 /*=====显示=====*/
-void Display_List(struct athletes athle[], int number){
+void Display_List(athletes athle[], int number){
     printf("===display===\n");
-    for(int i=0;i<number;i++){
-        printf("%s   %s   %s   %s  %s   %s   %s ",athle[i].id,athle[i].name,athle[i].gender,athle[i].major,athle[i].college,athle[i].grade,athle[i].preteam);
+    for(int i=0;i<=number;i++){
+        printf("%s   %s   %s   %s  %s   %s   %s",athle[i].id,athle[i].name,athle[i].gender,athle[i].major,athle[i].college,athle[i].grade,athle[i].preteam);
+        printf("\n");
     }
     printf("\n");
 }
 
-int getFindTemp()
-{
-    return findtemp;
-}
